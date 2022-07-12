f4d077202890d46d56a0fdb0ec9a5b74ed35e9c5
Charle Rae <charlerae@Charles-MacBook-Air.local>
2022-07-12 10:05:28 -0400

    fetching quotes from api

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello There Friend!!');
+
+
+let apiQuotes = [];
+
+// get quotes from api
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// on load
+getQuotes();
+
